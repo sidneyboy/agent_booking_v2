f7e47fd1880da38ba4b0b0eea0f8a19e3bdf1636
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2023-05-11 08:10:26 +0800

    updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -498,13 +498,13 @@
         </div>
         <!-- /.content-wrapper -->
         @section('footer')
-            <footer class="main-footer">
+            {{-- <footer class="main-footer">
                 <div class="float-right d-none d-sm-block">
                     <b>Version</b> 3.0.2
                 </div>
                 <strong>Copyright &copy; 2022 Julmar Commercial Inc All rights
                     reserved.
-            </footer>
+            </footer> --}}
             <!-- Control Sidebar -->
             <aside class="control-sidebar control-sidebar-dark">
                 <!-- Control sidebar content goes here -->
